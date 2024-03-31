5d4a5c156b1634b0945e68e26f2f45bb8422f889
msali50 <116566903+msali50@users.noreply.github.com>
2024-03-31 21:09:21 +0100

    Add files via upload
    This is version 4 of the userProfile which has been RE-WORKED.
    
    I have remade the HTML, CSS and JavaScript in attempt to be able to show each question section independantly, as well as added a rough frame for input validation to be used in the JS file.
    
    In this version, I have also coded the JavaScript for the userProfile to work with arrays for the each sections questions, where prior versions used scanned the span elements in the HTML file for their contents.
    
    In its current form, the HTML file may require tweaking, but most of the work needs to be done to the JavaScript file which only has section 1 roughly working.
    
    To do:
    - Implement use of section 2 and section 3 in the 'answerQuestions' function.
    - Implement input validation for all sections, for inputs that require them.
    - Implement input sanitisation if required.
    - Create function to append valid inputs to array and to update HTML page/file.
    - Add a the ability to answer unanswered questions.
    - Make sure all sections are presented together at the end.
    - Add a button to Edit Answers
    - Add a function to edit answers if required.
    - Add a link to a 'page editor' webpage.
    - Add a link to the websites 'home page'.
    - Update CSS to make website seem cohesive; e.g. adding background image etc.

--- userProfileJS_LuxeV.js
+++ userProfileJS_LuxeV.js
@@ -1,105 +1,103 @@
-let numberOfQuestions = 10
+const sectionTitles = ["Personal details", "Contact information", "Home search preferences"];
+const section1Questions = ["your FIRST NAME", "your SURNAME", "your DATE OF BIRTH"];
+const section2Questions = ["your HOME ADDRESS", "your PHONE NUMBER", "your EMAIL"];
+const section3Questions = ["a desired HOUSE PRICE", "a desired HOUSE LOCATION", "the desired NUMBER OF BEDROOMS", "if ELECTRIC CAR CHARGING is required for your home: (yes/no)"];
+let section1Answers = [];
+let section2Answers = [];
+let section3Answers = [];
 let progressValue = 0;
-let allAnswered = true;
+let currentSection = 1; 
 
+
+// NOTE TO SELF - I need to adjust this funciton so it runs for each seperate section, when a button is pressed.
+// NOTE TO SELF - complete function as required.
 function answerQuestions(){
-    /* This function scans all the span elements (user answers to questions), to see if they are empty, signifying no answer was provided.
-    If this is the case, the function 'questionPromt' is called upon.*/
+    for (i = 0; i < sectionTitles.length; i++){
+        switch(i + 1){
+            case 1:
+                console.log("Searching section (" + (i + 1) + ")");
+                sectionQuestions = section1Questions;
+                sectionAnswers = section1Answers;
 
-    /* 'allAnswered' here, resets allAnswered to true when the button is pressed again;
-    All questions are marked as answered, before question are serched to see if they have been answered.*/
-    allAnswered = true;
+                for (k = 0; k < sectionQuestions.length; k++){
+                    console.log("Searching question (" + (k + 1) + ") in section (" + (i + 1) + ")");
 
-    for (let i = 0; i < numberOfQuestions; i++){
-        console.log("For loop iteration: " + i);
-        if (document.getElementById("answer" + (i+1)).innerText == "" || document.getElementById("answer" + (i+1)).classList.contains("unanswered")){
-            console.log(" The span tag with id = answer" + (i+1) + " ,is unanswered! Here is it's contents (" + document.getElementById("answer" + (i+1)).innerText + ")");
-            // 'allAnswered' here, is set to false; showing all questions have NOT been answered.
-            allAnswered = false;
-            questionPromt(i+1);
-        }
-        else {
-            console.log("The span tag with id = answer" + (i+1) + " ,is ANSWERED, or does not exist! Here is it's contents (" + document.getElementById("answer" + (i+1)).innerText + ")");
+                    // NOTE TO SELF - Do I need to adjust this to work properly with the array!?!?!?!
+                    if (sectionAnswers[k] == "" || sectionAnswers[k] == null){
+                        console.log("question(" + (k + 1) + ") [index (" + k + ")], is unanswered!")
+                        userInput = prompt(sectionQuestions[k]);
+                        checkUserInput(userInput, sectionQuestions[k]);
+                    }
+                    else{
+                        console.log("question (" + k + "), has been ANSWERED!")
+                    }
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
         }
     }
-    profileComplete();
 }
 
-function questionPromt(questionNum){
-    /* This function when called upon, presents a promt to the user for unanswered quesitons, which is determined by this functions input 'question'.
-    'question' is a number representing the question number for the user profile that is unanswered.*/
-    let userInput = null;
-    switch(questionNum){
-        case 1:
-            // promt for question 1.
-            userInput = prompt("Personal details | question 1/3 \n\n Please enter your FIRST NAME:");
-            break;
-        case 2:
-            // promt for question 2.
-            userInput = prompt("Personal details | question 2/3 \n\n Please enter your SURNAME:");
-            break;
-        case 3:
-            // promt for question 3.
-            userInput = prompt("Personal details | question 3/3 \n\n Please enter your DATE OF BIRTH:");
-            break;
-        case 4:
-            // promt for question 4.
-            userInput = prompt("Contact information | step 1/3 \n\n Please enter your HOME ADDRESS:");
-            break;
-        case 5:
-            // promt for question 5.
-            userInput = prompt("Contact information | step 2/3 \n\n Please enter your PHONE NUMBER:");
-            break;
-        case 6:
-            // promt for question 6.
-            userInput = prompt("Contact information | step 3/3 \n\n Please enter your EMAIL:");
-            break;
-        case 7:
-            // promt for question 7.
-            userInput = prompt("Home search preferences | step 1/4 \n\n Please enter a desired HOUSE PRICE:");
-            break;
-        case 8:
-            // promt for question 8.
-            userInput = prompt("Home search preferences | step 2/4 \n\n Please enter a desired HOUSE LOCATION:");
-            break;
-        case 9:
-            // promt for question 9.
-            userInput = prompt("Home search preferences | step 3/4 \n\n Please enter the desired NUMBER OF BEDROOMS:");
-            break;
-        case 10:
-            // promt for question 10.
-            userInput = prompt("Home search preferences | step 4/4 \n\n Please enter if ELECTRIC CAR CHARGING is required for your home: (yes/no)");
-            break;
-    }
-    questionAnswered(userInput, questionNum);
-}
-
-function questionAnswered (userInput, questionNum){
-    /* This function runs after the 'questionPrompt' function, and checks the promt's input box for whether a user input was provided or not.
-    If there is an input, the corresponding question answered is updated, and the profile completion progress bar's value is incremented/increased.*/
-    console.log("The userInput was (" + userInput + "), for question number (" + questionNum + ")");
+function checkUserInput(userInput, sectionQuestion){
+    console.log("Checking the user input (" + userInput + "), for question (" + sectionQuestion + ")")
     if (userInput != "" && userInput != null){
-        // Here the answer is updated with the user input, and is removed from the 'unanswered' class if it is a part of it.
-        document.getElementById("answer" + (questionNum)).classList.remove("unanswered");
-        document.getElementById("answer" + (questionNum)).innerHTML = userInput;
 
-        // The progress bar's value and the questions answered counter below it, is updated to reflect answered question.
-        progressValue += 1;
-        document.getElementById("profileProgressBar").value = progressValue;
-        document.getElementById("questionCounter").innerHTML = progressValue + " / " + numberOfQuestions;
-        console.log("The user input for question number (" + questionNum + "), was updated successfully!");
+        // This switch is used to determine the current Section being answered.
+        switch(currentSection){
+
+            // Case 1 - is for Section 1
+            case 1:
+                console.log("validation for section (" + currentSection + ")")
+                // This switch is used to determine the question being answered within Section 1 and the appropriate input validation for the question (if required).
+                switch(sectionQuestion){
+                    case section1Questions[0]:
+                        break;
+                    case section1Questions[1]:
+                        break;
+                    case section1Questions[2]:
+                        break;       
+                }
+            break;
+            
+            // Case 2 - is for Section 2
+            case 2:
+                // This switch is used to determine the question being answered within Section 2 and the appropriate input validation for the question (if required).
+                switch(sectionQuestion){
+                    case section2Questions[0]:
+                        break;
+                    case section2Questions[1]:
+                        break;
+                    case section2Questions[2]:
+                        break;       
+                }
+            break;
+
+            // Case 3 - is for Section 3
+            case 3:
+                // This switch is used to determine the question being answered within Section 3 and the appropriate input validation for the question (if required).
+                switch(sectionQuestion){
+                    case section3Questions[0]:
+                        break;
+                    case section3Questions[1]:
+                        break;
+                    case section3Questions[2]:
+                        break;
+                    case section3Questions[3]:
+                        break; 
+                }
+            break;
+        }
     }
     else{
-        // Unanswered questions are marked with 'Not answered', and are added to the 'unanswered' class that has its own style seperate from answered questions.
-        document.getElementById("answer" + (questionNum)).innerHTML = "Not answered!";
-        document.getElementById("answer" + (questionNum)).classList.add("unanswered");
-        console.log("The user did not provide an input/answer.");
+        console.log("The userInput (" + userInput + "), for question (" + sectionQuestion + ") is EMPTY!!!");
     }
 }
 
-function profileComplete(){
-    // This function checks if all the questions have been answered.
-    if (allAnswered == true){
-        alert("All the questions have been answered!");
-    }
+
+
+function handleSection1(){
+    document.getElementsByClassName("section1").style.display = "inline";
 }
